d38c407d239213aecc6fc98c8c1df24ab8800bf7
= <=>
2020-07-25 23:10:38 +0700

    Doing GUI

--- ManageCafe/ManageCafe/WpfMain.xaml.cs
+++ ManageCafe/ManageCafe/WpfMain.xaml.cs
@@ -23,5 +23,43 @@ namespace ManageCafe
         {
             InitializeComponent();
         }
+        private int _numValue = 0;
+
+        public int NumValue
+        {
+            get { return _numValue; }
+            set
+            {
+                _numValue = value;
+                txtNum.Text = value.ToString();
+            }
+        }
+
+        public void NumberUpDown()
+        {
+            InitializeComponent();
+            txtNum.Text = _numValue.ToString();
+        }
+
+        private void cmdUp_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue++;
+        }
+
+        private void cmdDown_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue--;
+        }
+
+        private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtNum == null)
+            {
+                return;
+            }
+
+            if (!int.TryParse(txtNum.Text, out _numValue))
+                txtNum.Text = _numValue.ToString();
+        }
     }
 }
